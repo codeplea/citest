1a348858ffba954ac957fb1aced57428bd5c39f6
Lewis Van Winkle <lv@codeplea.com>
2018-08-29 14:01:36 -0500

    typo

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 SRC=id
 
 cc -Wall -Wextra ${SRC}.c -o ${SRC}; ./${SRC}; rm ${SRC}
-cpp -Wall -Wextra ${SRC}.c -o ${SRC}; ./${SRC}; rm ${SRC}
+c++ -Wall -Wextra ${SRC}.c -o ${SRC}; ./${SRC}; rm ${SRC}
