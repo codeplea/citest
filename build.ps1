a54cd340471cdb214a7b9e288c604f6ca9f65325
Lewis Van Winkle <lv@codeplea.com>
2018-08-29 16:42:00 -0500

    added mingw

--- build.ps1
+++ build.ps1
@@ -1,11 +1,22 @@
+copy id.c id.cpp
 
 cmd /c 'cl id.c 2>&1'
 ./id.exe
 del ./id.exe
 
-copy id.c id.cpp
 cmd /c 'cl id.cpp 2>&1'
 ./id.exe
 del ./id.exe
-del id.cpp
 
+
+& "C:\MinGW\bin\gcc" id.c -o id.exe
+./id.exe
+del ./id.exe
+
+& "C:\MinGW\bin\gcc" id.cpp -o id.exe
+./id.exe
+del ./id.exe
+
+
+
+del id.cpp
