81c6d02c534374dcb5f99162f6eccd1acff6af5d
Lewis Van Winkle <lv@codeplea.com>
2018-08-29 12:18:01 -0500

    try

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # citest
-just testing
+
+This is a small test. The goal is to compile and run the C file,
+`id.c`, on many different platforms and compilers, using the free
+continuous integration services available.
---
+++ id.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+    printf("\n");
+    printf("os: ");
+#if defined(_WIN32)
+    printf("windows\n");
+#elif defined(__linux__)
+    printf("linux\n");
+#elif defined(__APPLE__)
+    printf("osx\n");
+#else
+    printf("unknown\n");
+#endif
+
+
+    printf("compiler: ");
+#if defined(_MSC_VER)
+#if _MSC_VER == 1800
+    printf("mvs 2013\n");
+#elif _MSC_VER == 1900
+    printf("mvs 2015\n");
+#elif _MSC_VER >= 1910 && _MSC_VER <= 1914
+    printf("mvs 2017\n");
+#else
+    printf("mvs unknown\n");
+#endif
+#elif defined(__clang__)
+    printf("clang\n");
+#elif defined(__GNUC__)
+    printf("gcc\n");
+#else
+    printf("unknown\n");
+#endif
+
+
+printf("language: ");
+#ifdef __cplusplus
+    printf("c++\n");
+#else
+    printf("c\n");
+#endif
+
+    printf("pointer size: ");
+    printf("%d\n", sizeof(void*));
+
+    printf("\n");
+    return 0;
+}
