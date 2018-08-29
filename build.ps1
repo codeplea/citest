c1f7b52ffa10ce472b494fa02c0749db18a09f28
Lewis Van Winkle <lv@codeplea.com>
2018-08-29 15:22:45 -0500

    change cpp extension

--- build.ps1
+++ build.ps1
@@ -1,2 +1,11 @@
+
 cl id.c
 ./id
+del ./id
+
+copy id.c id.cpp
+cl id.cpp
+./id
+del ./id
+del id.cpp
+
--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 SRC=id
 
-for COMP in gcc g++ clang clang++
+for COMP in gcc clang
 do
     echo
     $COMP --version
     echo
+    cp ${SRC}.c ${SRC}.cpp
     $COMP -Wall -Wextra ${SRC}.c -o ${SRC}; ./${SRC}; rm ${SRC}
+    $COMP -Wall -Wextra ${SRC}.cpp -o ${SRC}; ./${SRC}; rm ${SRC}
+    rm ${SRC}.cpp
     echo
     echo
 done
