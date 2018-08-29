162dc562da504d27775bd1d9bd2df1a5fd2e2807
Lewis Van Winkle <lv@codeplea.com>
2018-08-29 15:35:19 -0500

    fix osx clang

--- build.sh
+++ build.sh
@@ -1,14 +1,20 @@
 SRC=id
 
-for COMP in gcc clang
-do
-    echo
-    $COMP --version
-    echo
-    cp ${SRC}.c ${SRC}.cpp
-    $COMP -Wall -Wextra ${SRC}.c -o ${SRC}; ./${SRC}; rm ${SRC}
-    $COMP -Wall -Wextra ${SRC}.cpp -o ${SRC}; ./${SRC}; rm ${SRC}
-    rm ${SRC}.cpp
-    echo
-    echo
-done
+if [[ ${TRAVIS_OS_NAME} = osx ]]; then
+    # Since default gcc on osx is just a front-end for LLVM
+    if [[ ${CC} = gcc ]]; then
+        export CXX=g++-4.8
+        export CC=gcc-4.8
+    fi
+fi
+
+
+echo
+${CC} --version
+echo
+cp ${SRC}.c ${SRC}.cpp
+${CC} -Wall -Wextra ${SRC}.c -o ${SRC}; ./${SRC}; rm ${SRC}
+${CC} -Wall -Wextra ${SRC}.cpp -o ${SRC}; ./${SRC}; rm ${SRC}
+rm ${SRC}.cpp
+echo
+echo
