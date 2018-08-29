3905856f8095100f86da7eeb78bc2f71ddc081a9
Lewis Van Winkle <lv@codeplea.com>
2018-08-29 15:52:51 -0500

    try once more

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
 SRC=id
 
-if [[ ${TRAVIS_OS_NAME} = osx ]]; then
-    # Since default gcc on osx is just a front-end for LLVM
-    if [[ ${CC} = gcc ]]; then
-        export CXX=g++-4.8
-        export CC=gcc-4.8
-    fi
-fi
-
+# https://github.com/travis-ci/travis-ci/issues/2423
+if [ "$TRAVIS_OS_NAME" = "osx" ] && [ "$CC" = "gcc" ]; then export CC=gcc-4.8; fi
 
 echo
 cp ${SRC}.c ${SRC}.cpp
