38972375ff4d1c4036421890dcfaf35957dab0b1
Lewis Van Winkle <lv@codeplea.com>
2018-08-29 15:39:30 -0500

    path test

--- build.sh
+++ build.sh
@@ -10,11 +10,13 @@ fi
 
 
 echo
-${CC} --version
-echo
 cp ${SRC}.c ${SRC}.cpp
 ${CC} -Wall -Wextra ${SRC}.c -o ${SRC}; ./${SRC}; rm ${SRC}
+echo
 ${CC} -Wall -Wextra ${SRC}.cpp -o ${SRC}; ./${SRC}; rm ${SRC}
 rm ${SRC}.cpp
 echo
 echo
+
+which gcc
+which clang
