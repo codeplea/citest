4f50107bcbc5477442d1b1be76b87d39cfbb9537
Lewis Van Winkle <lv@codeplea.com>
2018-08-29 13:54:35 -0500

    try again

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 SRC=id
 
-gcc -Wall -Wextra ${SRC}.c -o ${SRC}; ./${SRC}; rm ${SRC}
-g++ -Wall -Wextra ${SRC}.c -o ${SRC}; ./${SRC}; rm ${SRC}
-clang -Wall -Wextra ${SRC}.c -o ${SRC}; ./${SRC}; rm ${SRC}
-clang++ -Wall -Wextra ${SRC}.c -o ${SRC}; ./${SRC}; rm ${SRC}
+cc -Wall -Wextra ${SRC}.c -o ${SRC}; ./${SRC}; rm ${SRC}
+cpp -Wall -Wextra ${SRC}.c -o ${SRC}; ./${SRC}; rm ${SRC}
