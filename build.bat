d9cff87606c40e0918ff4b3ffab987cfdedfbabf
Lewis Van Winkle <lv@codeplea.com>
2018-08-29 21:22:42 -0500

    again

--- build.bat
+++ build.bat
@@ -1,14 +1,15 @@
 copy id.c id.cpp
 
 cl id.c
-./id.exe
-del ./id.exe
+id.exe
+del id.exe
 
 cl id.cpp
-./id.exe
-del ./id.exe
+id.exe
+del id.exe
 
 
+if "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2015" (
 "C:\MinGW\bin\gcc.exe" id.c -o id.exe
 id.exe
 del id.exe
@@ -16,7 +17,7 @@ del id.exe
 "C:\MinGW\bin\gcc.exe" id.cpp -o id.exe
 id.exe
 del id.exe
-
+)
 
 "C:\Program Files\LLVM\bin\clang.exe" id.c -o id.exe
 id.exe
@@ -26,10 +27,5 @@ del id.exe
 id.exe
 del id.exe
 
-
 del id.cpp
 
-dir
-
-dir "C:\Program Files\LLVM\bin"
-dir "C:\MinGW\bin"
